3427873a62c42c0aeb91dd1aa27cdaf4d0bc1af4
YKS <styleyo12@naver.com>
2022-02-04 13:39:02 +0900

    war을 위한 설정

--- src/main/java/com/project/board/BoardApplication.java
+++ src/main/java/com/project/board/BoardApplication.java
@@ -2,12 +2,19 @@ package com.project.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BoardApplication {
+public class BoardApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BoardApplication.class);
 	}
 
 }
